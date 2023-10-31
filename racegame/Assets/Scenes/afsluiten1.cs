1a94b5ed2fb6653ecc5779e3a300613dd8540b68
ppcsx <117719577+ppcsx@users.noreply.github.com>
2023-10-31 14:39:05 +0100

    ee

---
+++ racegame/Assets/Scenes/afsluiten1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class afsluiten1 : MonoBehaviour
+{ 
+    
+    public void suckdjj()
+    {
+    UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+}
