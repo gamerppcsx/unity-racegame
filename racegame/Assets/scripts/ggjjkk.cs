a89a7214c8375d10500aaf7f0aad4f5f4a374f5a
ppcsx <117719577+ppcsx@users.noreply.github.com>
2023-11-09 17:08:19 +0100

    fg

---
+++ racegame/Assets/scripts/ggjjkk.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ggjjkk : MonoBehaviour
+{
+    public Transform kgm;
+    public Transform kgm2;
+
+
+    void Update()
+    {
+        kgm.Rotate(0, 0.2f, 0);
+        kgm2.Rotate(0, 0, 90);
+    }
+}
