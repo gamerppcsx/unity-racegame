cf80b75796af2309d50a4c3e8066794376135bb3
gamerppcsx <146166259+gamerppcsx@users.noreply.github.com>
2023-11-16 09:33:55 +0100

    erg

--- racegame/Assets/scripts/switch.cs
+++ racegame/Assets/scripts/switch.cs
@@ -9,6 +9,7 @@ public class SceneSwitcher : MonoBehaviour
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("secondairy");
+    Time.timeScale = 1f;
     }
     public void menu1()
     {
@@ -16,53 +17,64 @@ public class SceneSwitcher : MonoBehaviour
 	SceneManager.LoadScene("menu1");
     }
         public void menu2()
+        
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1, LoadSceneMode.Additive);
 	SceneManager.LoadScene("menu2");
+    Time.timeScale = 1f;
     }
         public void playgame2()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("secondairy 2");
+    Time.timeScale = 1f;
     }
             public void menu3()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("menu3");
+    Time.timeScale = 1f;
     }
             public void playgame3()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("secondairy 3");
+    Time.timeScale = 1f;
     }
                 public void playgame4()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("secondairy 4");
+    Time.timeScale = 1f;
     }
                 public void menu4()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("menu4");
+    Time.timeScale = 1f;
     }
                 public void playgame5()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("secondairy 5");
+    Time.timeScale = 1f;
     }
                     public void menu5()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("menu5");
+    Time.timeScale = 1f;
     }
                         public void controles()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("controles");
+    Time.timeScale = 1f;
     }
                             public void menuxd()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
 	SceneManager.LoadScene("menuXD");
+    Time.timeScale = 1f;
     }
 }
